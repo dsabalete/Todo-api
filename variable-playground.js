0295d3ff753eea5c0b7b25b5ddfa01b71553fee0
David Sabalete <dsabalete@gmail.com>
2016-09-01 10:47:58 +0200

    Add variable-playground.js

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+// var person = {
+//     name: 'Andrew',
+//     age: 21
+// };
+
+// function updatePerson(obj) {
+//     obj.age = 24;
+
+// }
+
+// updatePerson(person);
+
+// console.log(person);
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+    gradesArr.push(55);
+    debugger;
+    //gradesArr  = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
