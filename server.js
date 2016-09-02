de05a8b4bdfd35a20d03d6814c0c2e27fffb1a01
David Sabalete <dsabalete@gmail.com>
2016-09-02 13:59:55 +0200

    Add POST /users/login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -122,10 +123,33 @@ app.post('/users', function (req, res) {
 
     db.user.create(body).then(function (user) {
         res.json(user.toPublicJSON());
-    }, function(e) {
+    }, function (e) {
         res.status(400).json(e);
     });
 
+});
+
+// POST /users/login
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+        res.status(400).send();
+    }
+
+    db.user.findOne({
+        where: {
+            email: body.email
+        }
+    }).then(function (user) {
+        if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+            return res.status(401).send();
+        }
+        
+        res.json(user.toPublicJSON());
+    }, function (e) {
+        res.status(500).send();
+    });
 });
 
 db.sequelize.sync().then(function () {
